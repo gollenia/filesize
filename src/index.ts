cdad195dcaf023d0c3ea7e332c3bb4999ee101f5
Thomas Gollenia <thomas@gollenia.at>
2022-11-17 20:06:20 +0100

    physical added

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 type FileSize = {
 	value: number;
-	unit: 'byte' | 'kilobyte' | 'megabyte' | 'gigabyte' | 'terrabyte';
+	unit: 'byte' | 'kilobyte' | 'megabyte' | 'gigabyte' | 'terrabyte' | 'petabyte' | 'exabyte';
 };
 
-type Units = ('byte' | 'kilobyte' | 'megabyte' | 'gigabyte' | 'terrabyte')[];
+type Units = ('byte' | 'kilobyte' | 'megabyte' | 'gigabyte' | 'terrabyte' | 'petabyte' | 'exabyte')[];
 
-const fileSize = (bytes: number): FileSize => {
-	const thresh = 1024;
+const fileSize = (bytes: number, physical: boolean = false): FileSize => {
+
+	const threshold = physical ? 1000 : 1024;
 
 	const size: FileSize = {
 		value: bytes,
 		unit: 'byte',
 	};
 
-	if (Math.abs(bytes) < thresh) return { value: bytes, unit: 'byte' };
+	if (Math.abs(bytes) < threshold) return size;
 
-	const units: Units = ['kilobyte', 'megabyte', 'gigabyte', 'terrabyte'];
+	const units: Units = ['kilobyte', 'megabyte', 'gigabyte', 'terrabyte', 'petabyte', 'exabyte'];
 	let u: number = -1;
 	const r: number = 10 ** 1;
 
 	do {
-		bytes /= thresh;
+		bytes /= threshold;
 		++u;
-	} while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+	} while (Math.round(Math.abs(bytes) * r) / r >= threshold && u < units.length - 1);
 
 	size.value = bytes;
 	size.unit = units[u];
