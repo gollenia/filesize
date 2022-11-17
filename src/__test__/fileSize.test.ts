64a8056b60b3af71f9c8bdc9c3b0160fc2c8e008
Thomas Gollenia <thomas@gollenia.at>
2022-11-17 20:06:32 +0100

    new test for physical size

--- src/__test__/fileSize.test.ts
+++ src/__test__/fileSize.test.ts
@@ -3,3 +3,7 @@ import fileSize from "..";
 test('File Size', () => {
 	expect(fileSize(1024)).toStrictEqual({ unit: 'kilobyte', value: 1 })
 })
+
+test('File Size physical', () => {
+	expect(fileSize(1000, true)).toStrictEqual({ unit: 'kilobyte', value: 1 })
+})
