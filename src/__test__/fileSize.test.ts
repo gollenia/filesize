912e08fae3bafea22bf7dd374f72a847468bd99e
Thomas Gollenia <thomas@gollenia.at>
2022-11-17 20:08:47 +0100

    prettified

--- src/__test__/fileSize.test.ts
+++ src/__test__/fileSize.test.ts
@@ -1,9 +1,9 @@
-import fileSize from "..";
+import fileSize from '..';
 
 test('File Size', () => {
-	expect(fileSize(1024)).toStrictEqual({ unit: 'kilobyte', value: 1 })
-})
+  expect(fileSize(1024)).toStrictEqual({ unit: 'kilobyte', value: 1 });
+});
 
 test('File Size physical', () => {
-	expect(fileSize(1000, true)).toStrictEqual({ unit: 'kilobyte', value: 1 })
-})
+  expect(fileSize(1000, true)).toStrictEqual({ unit: 'kilobyte', value: 1 });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
 type FileSize = {
-	value: number;
-	unit: 'byte' | 'kilobyte' | 'megabyte' | 'gigabyte' | 'terrabyte' | 'petabyte' | 'exabyte';
+  value: number;
+  unit: 'byte' | 'kilobyte' | 'megabyte' | 'gigabyte' | 'terrabyte' | 'petabyte' | 'exabyte';
 };
 
 type Units = ('byte' | 'kilobyte' | 'megabyte' | 'gigabyte' | 'terrabyte' | 'petabyte' | 'exabyte')[];
 
 const fileSize = (bytes: number, physical: boolean = false): FileSize => {
+  const threshold = physical ? 1000 : 1024;
 
-	const threshold = physical ? 1000 : 1024;
+  const size: FileSize = {
+    value: bytes,
+    unit: 'byte',
+  };
 
-	const size: FileSize = {
-		value: bytes,
-		unit: 'byte',
-	};
+  if (Math.abs(bytes) < threshold) return size;
 
-	if (Math.abs(bytes) < threshold) return size;
+  const units: Units = ['kilobyte', 'megabyte', 'gigabyte', 'terrabyte', 'petabyte', 'exabyte'];
+  let u: number = -1;
+  const r: number = 10 ** 1;
 
-	const units: Units = ['kilobyte', 'megabyte', 'gigabyte', 'terrabyte', 'petabyte', 'exabyte'];
-	let u: number = -1;
-	const r: number = 10 ** 1;
+  do {
+    bytes /= threshold;
+    ++u;
+  } while (Math.round(Math.abs(bytes) * r) / r >= threshold && u < units.length - 1);
 
-	do {
-		bytes /= threshold;
-		++u;
-	} while (Math.round(Math.abs(bytes) * r) / r >= threshold && u < units.length - 1);
-
-	size.value = bytes;
-	size.unit = units[u];
-	return { value: bytes, unit: units[u] };
+  size.value = bytes;
+  size.unit = units[u];
+  return { value: bytes, unit: units[u] };
 };
 
 export default fileSize;
